a295e5e780bc13993b9a7d56402518a81e02e068
Rahim Alizada <rahim.alizada@gmail.com>
2021-08-25 11:48:53 +0400

    Updated account models

--- projects/rahimalizada/ngx-common/src/lib/model/account/abstract-account.service.ts
+++ projects/rahimalizada/ngx-common/src/lib/model/account/abstract-account.service.ts
@@ -1,14 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AbstractRestService } from '../../rest/abstract-rest.service';
 import { AbstractAccount } from './abstract-account.model';
 import { AccountRole } from './account-role.model';
 import { AuthResult } from './auth-result.model';
 
-@Injectable({
-  providedIn: 'root',
-})
 export class AbstractAccountService<T extends AbstractAccount<AccountRole>> extends AbstractRestService<T> {
   constructor(httpClient: HttpClient, basePath: string) {
     super(httpClient, basePath);
