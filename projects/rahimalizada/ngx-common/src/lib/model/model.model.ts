71a7ce16855298fb5bb1076be7ec253c183f9158
Rahim Alizada <rahim.alizada@gmail.com>
2021-09-05 11:21:43 +0400

    Updated model interfaces

---
+++ projects/rahimalizada/ngx-common/src/lib/model/model.model.ts
@@ -0,0 +1,9 @@
+import { WithBranch } from './with-branch.model';
+import { WithID } from './with-id.model';
+
+export interface Model<S, T> extends WithID<S>, WithBranch<T> {
+  id: S;
+  created: Date;
+  updated: Date;
+  notes?: string;
+}
