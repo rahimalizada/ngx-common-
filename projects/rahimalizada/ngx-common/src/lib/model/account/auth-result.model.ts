2cf737fc17703fbd89b9370256082b44bdb5d5f1
Rahim Alizada <rahim.alizada@gmail.com>
2021-09-06 13:53:36 +0400

    Updated AbstractAccount model

--- projects/rahimalizada/ngx-common/src/lib/model/account/auth-result.model.ts
+++ projects/rahimalizada/ngx-common/src/lib/model/account/auth-result.model.ts
@@ -1,6 +1,6 @@
 import { AbstractAccount } from './abstract-account.model';
 
-export interface AuthResult<T extends AbstractAccount<any>> {
+export interface AuthResult<T extends AbstractAccount<unknown, unknown>> {
   subject: T;
   token: string;
   refreshToken: string;
