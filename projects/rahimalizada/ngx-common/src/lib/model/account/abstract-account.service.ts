598ae6dfd7287b532385c5cfb40886b4a92401b7
Rahim Alizada <rahim.alizada@gmail.com>
2021-09-09 15:28:41 +0400

    Updated AbstractAccountService

--- projects/rahimalizada/ngx-common/src/lib/model/account/abstract-account.service.ts
+++ projects/rahimalizada/ngx-common/src/lib/model/account/abstract-account.service.ts
@@ -1,23 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AbstractAuthService } from '../../auth/abstract-auth.service';
 import { AbstractRestService } from '../../rest/abstract-rest.service';
 import { AbstractAccount } from './abstract-account.model';
 import { AuthResult } from './auth-result.model';
+import { PasswordChangeRequest } from './password-change-request.model';
 
 export class AbstractAccountService<S extends AbstractAccount<T, U>, T, U> extends AbstractRestService<S> {
   constructor(httpClient: HttpClient, basePath: string) {
     super(httpClient, basePath);
   }
 
-  changePassword(value: { currentPassword: string; newPassword: string }): Observable<AuthResult<S>> {
-    return this.httpClient.post<AuthResult<S>>(`${this.basePath}/password`, value);
+  changePassword(value: PasswordChangeRequest, recaptchaSiteKey: string, recaptchaConfirmation: string): Observable<AuthResult<S>> {
+    return this.httpClient.post<AuthResult<S>>(
+      `${this.basePath}/password`,
+      value,
+      AbstractAuthService.buildRecaptchaOptions(recaptchaSiteKey, recaptchaConfirmation),
+    );
   }
 
   resendActivationCode(): Observable<void> {
     return this.httpClient.post<void>(`${this.basePath}/resend-activation-code`, {});
   }
 
-  activateAccount(code: string): Observable<AuthResult<S>> {
-    return this.httpClient.post<AuthResult<S>>(`${this.basePath}/activate`, code);
+  activateAccount(code: string, recaptchaSiteKey: string, recaptchaConfirmation: string): Observable<AuthResult<S>> {
+    return this.httpClient.post<AuthResult<S>>(
+      `${this.basePath}/activate`,
+      code,
+      AbstractAuthService.buildRecaptchaOptions(recaptchaSiteKey, recaptchaConfirmation),
+    );
   }
 }
